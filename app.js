00b233669d233437fdaeb50654371af11167f121
omerfarooqBM <50845983+omerfarooqBM@users.noreply.github.com>
2021-06-16 19:47:20 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 	function calcNumbers(result){
-		form.displayResult.value +=result;
+		console.log(result)
+		// form.displayResult.value +=result;
+		var lastVal=form.displayResult.value.slice(form.displayResult.value.length -1);
+		var opt = ['+','-','*','/'];
+		if(opt.indexOf(lastVal) !== -1 && opt.indexOf(result) !== -1){
+			form.displayResult.value=form.displayResult.value.slice(0,-1)+result
+
+		}else{
+			form.displayResult.value +=result
+		}
+
 		
 	}
 
@@ -11,5 +21,4 @@
 	function cl(){
 		form.displayResult.value =form.displayResult.value.slice(0, - 1);
 	}
-
 
